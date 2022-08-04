9f5cf74dc6c937a11ede0579e371177736a4295d
agustinsetya <agustin.setyo@majoo.id>
2022-08-04 14:33:00 +0700

    Added function to get all user

--- dto/User.go
+++ dto/User.go
@@ -1,7 +1,28 @@
 package dto
 
+import (
+	"fp2022-crm-sebagian-team/config"
+)
+
 type User struct {
+	Id		 string `json:"id" form:"id"`
 	Name     string `json:"name" form:"name"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"-" form:"password"`
+}
+
+var Users []User
+
+func All() ([]User, error) {
+	if err := config.DB.Find(&Users).Error; err != nil {
+		return nil, err
+	}
+	return Users, nil
+}
+
+func GetUserById(id string) error {
+	if err := config.DB.Where("id = ?", id).Take(&Users).Error; err != nil {
+		return err
+	}
+	return nil
 }
