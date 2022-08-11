7cd5037a7df2598730a0bc9b6368c9bfb30d57b1
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2022-08-11 15:31:22 +0700

    update gorm user

--- modules/user/user_entity.go
+++ modules/user/user_entity.go
@@ -13,15 +13,15 @@ func (User) TableName() string {
 type User struct {
 	gorm.Model
 	ID         int64        `gorm:"primaryKey;autoIncrement"`
-	Name       string       `gorm:"column:name"`
-	Email      string       `gorm:"column:email"`
-	Password   string       `gorm:"column:password"`
-	Address    string       `gorm:"column:address"`
-	IdPosition int64        `gorm:"column:id_position"`
-	CreatedAt  time.Time    `gorm:"column:created_at"`
-	CreatedBy  string       `gorm:"column:created_by"`
-	UpdatedAt  time.Time    `gorm:"column:updated_at"`
-	UpdatedBy  string       `gorm:"column:updated_by"`
+	Name       string       `gorm:"column:name;not null;type:varchar(50);"`
+	Email      string       `gorm:"column:email;not null;type:varchar(50);"`
+	Password   string       `gorm:"column:password;not null;type:varchar(100);"`
+	Address    string       `gorm:"column:address;not null;type:varchar(255);"`
+	IdPosition int64        `gorm:"column:id_position;not null;"`
+	CreatedAt  time.Time    `gorm:"column:created_at;not null;"`
+	CreatedBy  string       `gorm:"column:created_by;not null;type:varchar(50);"`
+	UpdatedAt  time.Time    `gorm:"column:updated_at;not null;"`
+	UpdatedBy  string       `gorm:"column:updated_by;type:varchar(50);"`
 	DeletedAt  sql.NullTime `gorm:"column:deleted_at"`
-	DeletedBy  string       `gorm:"column:deleted_by"`
+	DeletedBy  string       `gorm:"column:deleted_by;type:varchar(50);"`
 }
